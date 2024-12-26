31e13d7233db4dd0f556d4770e287bb1f2c7f9fc
june-dt <juneee.dt@gmail.com>
2024-12-27 02:53:49 +0800

    added camera selection submenu

--- main.py
+++ main.py
@@ -6,51 +6,76 @@ import ctypes
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk, messagebox, Menu
+from cv2_enumerate_cameras import enumerate_cameras
+
+import cv2
 
 from PIL import Image, ImageTk
 
-# variables
-isCameraOn = False
+# TODO: implement camera interface function
 
-# Create and initialize the main application window
+# Declaring/initializing variables
+isCameraOn = False
+cameraCounter = 0
+
+
+# Declaring/initializing variables
+def on_closing():
+    if messagebox.askyesno("Exit", "Do you want to exit?"):
+        root.destroy()
+
+
+def about_onclick():
+    messagebox.showinfo("About", "About")
+
+# def open_camera(camera_index):
+
+
+# Initializing main application window
 root = Tk()
 
 scrWidth = (root.winfo_screenwidth() // 1.5)
 scrHeight = (root.winfo_screenheight() // 1.5)
 
 # img = tk.PhotoImage(file="res/pcbDefect.png")
+
+# Initializing window parameters
 root.geometry("%dx%d+%d+%d" % (scrWidth, scrHeight, (scrWidth // 4), (scrHeight // 4)))
 root.resizable(False, False)
 root.title("pcbDefectProj")
 # root.iconphoto(False, img)
 
-# Menubar
+# Initializing Menu bar
 menubar = tk.Menu(root)
 
-menu_file = Menu(menubar, tearoff=0)
-menu_file.add_command(label="Close", command=root.quit)
+menu_properties = Menu(menubar, tearoff=0)
 
-menu_camerasource = tk.Menu(menubar, tearoff=0)
-menu_camerasource.add_radiobutton(label="source1", command='')
-menu_camerasource.add_radiobutton(label="source2", command='')
-menu_camerasource.add_separator()
-menu_camerasource.add_command(label="camera_disable", command='')
+submenu_cameraSource = Menu(menu_properties, tearoff=0)
 
+for camera_info in enumerate_cameras(cv2.CAP_MSMF):
+    cameraCounter += 1
+    submenu_cameraSource.add_radiobutton(label=f'{camera_info.name}', command='')
+    print(f'{camera_info.index}: {camera_info.name}')
 
-menu_help = tk.Menu(menubar, tearoff=0)
-menu_help.add_command(label="About...", command='')
+# submenu_cameraSource.add_radiobutton(label="source1", command='')
+# submenu_cameraSource.add_radiobutton(label="source2", command='')
+submenu_cameraSource.add_separator()
+submenu_cameraSource.add_command(label="camera_disable", command='')
 
-menubar.add_cascade(label="File", menu=menu_file)
-menubar.add_cascade(label="Camera Source", menu=menu_camerasource)
-menubar.add_cascade(label="Help", menu=menu_help)
+menu_properties.add_cascade(label="Camera Source", menu=submenu_cameraSource)
+
+menu_properties.add_separator()
+menu_properties.add_command(label="About App...", command=about_onclick)
+menu_properties.add_separator()
+menu_properties.add_command(label="Close", command=on_closing)
+
+menubar.add_cascade(label="Properties", menu=menu_properties)
 
 root.config(menu=menubar)
-
 
 # Canvas
 cameraFeed = tk.Canvas(root, borderwidth=2, relief="solid", bg="gray", height=(scrHeight - 20), width=(scrWidth - 20))
 cameraFeed.place(x=5, y=5)
-
 
 # Camera Feed Placeholder
 placeholder_camOn = ImageTk.PhotoImage(Image.open("res/camOn.png"))
@@ -68,7 +93,16 @@ btnCameraToggle.pack(side="bottom", pady=22.5)
 """
 
 # Close Button
-# root.protocol("WM_DELETE_WINDOW", close_btn_msgbox)
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Run the Tkinter event loop
 root.mainloop()
+
+
+"""
+import cv2
+from cv2_enumerate_cameras import enumerate_cameras
+
+for camera_info in enumerate_cameras(cv2.CAP_MSMF):
+    print(f'{camera_info.index}: {camera_info.name}')
+"""
